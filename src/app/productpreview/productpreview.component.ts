cc50c0aa76c14b71ab0cd29907bcc566b100b85e
Naga938 <53560208+Naga938@users.noreply.github.com>
2020-06-09 21:12:45 +0530

    productpreviewcartupdate

---
+++ src/app/productpreview/productpreview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productpreview',
+  templateUrl: './productpreview.component.html',
+  styleUrls: ['./productpreview.component.css']
+})
+export class ProductpreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
