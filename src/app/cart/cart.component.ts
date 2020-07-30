5e2f64e4cec01a709a6b86641d54c17de8991d3c
kowsalya A <kowsalya@arkhasystems.in>
2020-07-30 14:51:25 +0530

    Updated cart, admin product.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,6 +4,7 @@ import { CartService } from '@app/_services/cart.service';
 import { Cart } from '@app/_models/order/Cart';
 import { AccountService } from '@app/_services/account.service';
 import { User } from '@app/_models';
+import { CartProduct } from '@app/_models/newProduct/CartProduct';
 
 
 @Component({
@@ -12,7 +13,7 @@ import { User } from '@app/_models';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
+  cartProduct: CartProduct[]
   loggedIn: Boolean = false
   carts: Cart
   cartAvailable: Boolean = false
@@ -22,6 +23,9 @@ export class CartComponent implements OnInit {
     this.userLoggedInfo()
     this.getCartDetails()
 
+  }
+  calculateTotal(amount: number, quantity: number): number {
+    return amount * quantity
   }
 
   ngOnInit(): void {
@@ -43,9 +47,59 @@ export class CartComponent implements OnInit {
     this.carts = this.cartService.cartValue;
     if (this.carts != null && this.carts != undefined) {
       if (this.carts.products != null && this.carts.products != undefined && this.carts.products.length > 0) {
+        //for fake output cartAvailable is changed added else should be removed later
         this.cartAvailable = true
       }
     }
+    else {
+      //for fake output cartAvailable is changed to false
+      this.cartAvailable = true
+      this.cartProduct = [{
+        "id": "5f0c42d8f0f3f0768b7a2417",
+        "productTitle": "Hulk T-Shirt",
+        "title": "S",
+        "requiresShipping": true,
+        "taxable": true,
+        "available": false,
+        "name": "Hulk T-Shirt - S",
+        "price": 599.0,
+        "weight": 240.0,
+        "featuredImage": "http://localhost:8080/image/ullor-collections-hollywood-image4.jpg",
+        "quantity": 1,
+        "inventoryManagement": "AskTech",
+        "barcode": null
+      }, {
+        "id": "5f0c42d8f0f3f0768b7a2417",
+        "productTitle": "Hulk T-Shirt",
+        "title": "M",
+        "requiresShipping": true,
+        "taxable": true,
+        "available": false,
+        "name": "Hulk T-Shirt - M",
+        "price": 599.0,
+        "weight": 240.0,
+        "featuredImage": "http://localhost:8080/image/ullor-collections-hollywood-image4.jpg",
+        "quantity": 1,
+        "inventoryManagement": "AskTech",
+        "barcode": null
+      }, {
+        "id": "5f0c42d8f0f3f0768b7a2417",
+        "productTitle": "Hulk T-Shirt",
+        "title": "L",
+        "requiresShipping": true,
+        "taxable": true,
+        "available": false,
+        "name": "Hulk T-Shirt - L",
+        "price": 599.0,
+        "weight": 240.0,
+        "featuredImage": "http://localhost:8080/image/ullor-collections-hollywood-image4.jpg",
+        "quantity": 1,
+        "inventoryManagement": "AskTech",
+        "barcode": null
+      }]
+
+    }
+
   }
 
 }
