f4f142d28e2b3eb9a122ea4c144efd0153227547
AjithS321 <you@example.com>
2020-08-09 20:11:21 +0530

    cahnged environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,12 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:4000',
   userUrl: 'http://localhost:7071/api/ullormerchandise',
-  categoryUrl: 'http://localhost:7070/api/ullormerchandise',
+  //categoryUrl: 'http://localhost:7070/api/ullormerchandise',
+  categoryUrl: "https://9db9148959fb.ngrok.io/api/ullormerchandise",
   orderUrl: 'http://localhost:7072/api/ullormerchandise',
-  imageUrl: 'http://localhost:8080/image/upload'
+
+  //imageUrl: 'http://localhost:8080/image/upload'
+  imageUrl: 'https://50b531ff6242.ngrok.io/image/upload'
 };
 
 /*
